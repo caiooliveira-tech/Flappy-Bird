2869f4043df092dae2ae91965ae28dc2e5dd8994
Caio Oliveira <caio.oliveira07@gmail.com>
2024-05-05 20:32:26 -0300

    Gerador de Obstaculos adicionado

--- Assets/Scripts/GeradorDeObstaculos.cs
+++ Assets/Scripts/GeradorDeObstaculos.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class GeradorDeObstaculos : MonoBehaviour
 {
-
+    [SerializeField]
+    private float tempoParaGerar;
+    //Controla quanto tempo já passou para sabermos se já podemos gerar outro
+    private float cronometro;
+    [SerializeField]
+    private GameObject modeloObstaculo;
+    void Awake()
+    {
+        this.cronometro = this.tempoParaGerar;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.cronometro -= Time.deltaTime;
+        Debug.Log(cronometro);
+        if(this.cronometro <0) {
+            // Instantiate precisa do objeto que será instanciado, a posição e se ele será rotacionado ou não
+            GameObject.Instantiate(this.modeloObstaculo, this.transform.position,Quaternion.identity);
+            this.cronometro = this.tempoParaGerar;
+        }
     }
 }
