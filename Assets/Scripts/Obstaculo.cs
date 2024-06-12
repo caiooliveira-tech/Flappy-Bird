770bff1605acb944195038b1c81217dd0661a44a
Caio Oliveira <caio.oliveira07@gmail.com>
2024-06-12 13:55:04 -0300

    Pontuação Adicionada

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -9,15 +9,38 @@ public class Obstaculo : MonoBehaviour
 
     [SerializeField]
     private float variacaoY;
+
+    private Vector3 posicaoPassaro;
+
+    private bool pontuei;
+
+    private UIScript scriptDaUI;
+
     private void Awake()
     {
         this.transform.Translate(Vector3.up * Random.Range(-variacaoY,variacaoY));
     }
+    private void Start(){
+        this.posicaoPassaro = GameObject.FindObjectOfType<Bird>().transform.position;
+        this.scriptDaUI = GameObject.FindObjectOfType<UIScript>();
+    }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if(!this.pontuei && this.transform.position.x < posicaoPassaro.x){
+            Debug.Log("Pontuou");
+            this.pontuei = true;
+            this.scriptDaUI.adicionarPontos();
+        }
         this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
         
     }
+    void OnTriggerEnter2D(Collider2D collision){
+         this.Destruir();
+    }
+
+    void Destruir(){
+       Destroy(this.gameObject);
+     }
 }
